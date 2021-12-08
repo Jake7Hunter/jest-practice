b28d837d084ab7425ea391de683321bd6b9fd6b6
Jake Hunter <jake7hunter@hmail.com>
2021-12-08 02:57:37 -0500

    Ensured saved recipes can be deleted from Cli

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -28,21 +28,20 @@ describe('CookbookCli', () => {
 
   describe('Retrieving a recipe', () => {
     test('should display the ingredients required to make the specified recipe', () => {
-      const myNewCookbook = new Cookbook();
-      const myNewCookbookCli = new CookbookCli(myNewCookbook);
-
-      myNewCookbookCli.add(`fajitas`, [`chicken`, `tortillas`]);
-
-      const message = myNewCookbookCli.get(`fajitas`)
-
-      expect(message).toEqual(`The ingredients for fajitas are: chicken,tortillas`)
 
     });
   });
 
   describe('Deleting a recipe', () => {
     test('should accept the recipe name and display the correct message', () => {
+      const myNewCookbook = new Cookbook();
+      const myNewCookbookCli = new CookbookCli(myNewCookbook);
 
+      myNewCookbookCli.add(`fajitas`, [`chicken`, `tortillas`]);
+
+      const message = myNewCookbookCli.remove(`fajitas`);
+
+      expect(message).toEqual(`Successfully removed the following recipe: fajitas`);
     });
   });
 });
